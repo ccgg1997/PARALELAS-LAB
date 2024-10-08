26a909a361e360ee133468d4e5a79e7dfead29ce
Cristian Gómez <89625031+ccgg1997@users.noreply.github.com>
2024-10-08 05:03:20 +0000

    Actualizar versiones de Python en los archivos lab2.ipynb y lab2.2.ipynb

--- tarea-openmp/work/openmp-tutorial/Labs/Lab1/Exercises/.ipynb_checkpoints/exercise2-checkpoint.c
+++ tarea-openmp/work/openmp-tutorial/Labs/Lab1/Exercises/.ipynb_checkpoints/exercise2-checkpoint.c
@@ -1,28 +1,36 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
-    int Array[100];
     int max = 0;
     int min = 100000;
     int sum = 0;
     int avg;
+    int num_hilos = 10;
+    
+    srand(time(NULL));
 
-    #pragma omp parallel for reduction(max:max) reduction(min:min) reduction(+:sum) num_threads(100)
-   {
-        int i=omp_get_thread_num();
-        Array[i] = rand() % 10000;
-        if (Array[i] > max) {
-            max = Array[i];
+    #pragma omp parallel num_threads(num_hilos) reduction(max:max) reduction(min:min) reduction(+:sum)
+    {
+        int random_num;
+        
+        #pragma omp critical
+        {
+            random_num = rand() % 10000;
         }
-        if (Array[i] < min) {
-            min = Array[i];
+
+        if (random_num > max) {
+            max = random_num;
         }
-        sum += Array[i];
+        if (random_num < min) {
+            min = random_num;
+        }
+        sum += random_num;
     }
 
-    avg = sum / 100;
+    avg = sum / num_hilos;
 
     printf("Valor máximo: %d\n", max);
     printf("Valor mínimo: %d\n", min);
